f84a950f5d16b6ea7d3c94d6041d1f1312d4b1a0
Jevy <jevymira@outlook.com>
2025-03-13 10:26:57 -0700

    build: update Angular dependencies to ^19.1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,10 @@ import { WeatherForecast } from './weather-forecast';
 import { environment } from '../environments/environment.development';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    standalone: false
 })
 
 export class AppComponent implements OnInit {
