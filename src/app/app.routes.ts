5e689c191867fa71fd6736a6007aa0e761b3d33a
Jevy <jevymira@outlook.com>
2025-03-21 11:49:44 -0700

    feat: link id to offer-item component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { WeatherComponent } from './weather/weather.component';
 import { DesktopsComponent } from './desktops/desktops.component';
 import { LaptopsComponent } from './laptops/laptops.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { OfferItemComponent } from './offer-item/offer-item.component';
 
 export const routes: Routes = [
     { path: "weather", component: WeatherComponent },
     { path: "desktops", component: DesktopsComponent },
     { path: "laptops", component: LaptopsComponent },
     { path: "navbar", component: NavBarComponent },
+    { path: "offers/:id", component: OfferItemComponent},
     { path: "", component: WeatherComponent, pathMatch: "full" }
 ];
