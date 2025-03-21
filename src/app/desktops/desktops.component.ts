78bc8960ea0648593f63c7a10d50795707bd5fbd
Jevy <jevymira@outlook.com>
2025-03-20 19:10:23 -0700

    feat(desktop): fetch offers and display in table

--- src/app/desktops/desktops.component.ts
+++ src/app/desktops/desktops.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Offer } from '../offer';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment.development';
 
 @Component({
   selector: 'app-desktops',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './desktops.component.html',
   styleUrl: './desktops.component.scss'
 })
-export class DesktopsComponent {
+export class DesktopsComponent implements OnInit {
+  public offers: Offer[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getOffers();
+  }
+
+  getOffers() {
+    this.http.get<Offer[]>(`${environment.baseUrl}api/offers`).subscribe
+    (
+      {
+        next: result => this.offers = result,
+        error: error => console.error(error)
+      }
+    )
+  }
 }
---
+++ src/app/offer.ts
@@ -0,0 +1,5 @@
+export interface Offer {
+    id: number;
+    title: string;
+    url: string;
+}
