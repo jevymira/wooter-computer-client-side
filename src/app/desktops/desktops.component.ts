32ffc6f79b302873e48294f8235f6b7747f45b13
Jevy <jevymira@outlook.com>
2025-03-26 16:12:02 -0700

    refactor(desktops): move filter into sidebar

--- src/app/desktops/desktops.component.ts
+++ src/app/desktops/desktops.component.ts
@@ -1,34 +1,18 @@
-import { ChangeDetectionStrategy, Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Offer } from '../offer';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { environment } from '../../environments/environment.development';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { JsonPipe } from '@angular/common';
-import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-desktops',
   imports: [
-    RouterLink,
-    MatCheckboxModule,
-    FormsModule,
-    ReactiveFormsModule,
-    JsonPipe
+    RouterLink
   ],
   templateUrl: './desktops.component.html',
-  styleUrl: './desktops.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrl: './desktops.component.scss'
 })
 export class DesktopsComponent implements OnInit {
-  private readonly _formBuilder = inject(FormBuilder);
-
-  readonly _memory = this._formBuilder.group({
-    8: false,
-    16: false,
-    32: false
-  });
-
   public offers: Offer[] = [];
 
   constructor(private http: HttpClient) {}
--- src/app/side-bar/side-bar.component.ts
+++ src/app/side-bar/side-bar.component.ts
@@ -1,16 +1,30 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, inject } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { RouterOutlet } from '@angular/router';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { JsonPipe } from '@angular/common';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-side-bar',
   imports: [
     MatSidenavModule,
-    RouterOutlet
+    MatCheckboxModule,
+    RouterOutlet,
+    FormsModule,
+    ReactiveFormsModule,
+    JsonPipe
   ],
   templateUrl: './side-bar.component.html',
-  styleUrl: './side-bar.component.scss'
+  styleUrl: './side-bar.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SideBarComponent {
+  private readonly _formBuilder = inject(FormBuilder);
 
+  readonly _memory = this._formBuilder.group({
+    8: false,
+    16: false,
+    32: false
+  });
 }
