c86b1775b3047b47321cf53da78ff7ea939c3b9c
Jevy <jevymira@outlook.com>
2025-03-29 09:14:14 -0700

    feat: incl. memory filter(s) in query string (get)
    Cast router outlet data as an object with boolean properties, matching
    the selected form value(s) for filtering by memory capacity. Append to
    `memory` parameter in the `http.get` request.
    
    Default to including all values if none selected.

--- src/app/desktops/desktops.component.ts
+++ src/app/desktops/desktops.component.ts
@@ -15,21 +15,43 @@ import { AbstractControl, FormGroup } from '@angular/forms';
   styleUrl: './desktops.component.scss',
 })
 export class DesktopsComponent implements OnInit {
-  readonly _memory = inject(ROUTER_OUTLET_DATA) as Signal<AbstractControl>;
+  readonly _memory = inject(ROUTER_OUTLET_DATA) as Signal<Partial<{ has8: boolean | null; has16: boolean | null; has32: boolean | null; }>>;
   public offers: Offer[] = [];
+  // param = new HttpParams().set('category', 'Desktops');
 
   constructor(private http: HttpClient) {
     effect(() => {
-      console.log(this._memory());
+      let params = new HttpParams().set('category', 'Desktops')
+
+      // FIXME: Scope out a more extensible way of handling
+      // FormGroup value changes.
+      if (this._memory().has8) {
+        params = params.append('memory', '8');
+      }
+      if (this._memory().has16) {
+        params = params.append('memory', '16');
+      }
+      if (this._memory().has32) {
+        params = params.append('memory', '32');
+      }
+
+      // When no filter is selected, 
+      // default to retrieving all offer items.
+      if (!(params.has('memory'))) {
+        params = params.append('memory', '8');
+        params = params.append('memory', '16');
+        params = params.append('memory', '32');
+      }
+
+      this.getOffers(params);
     });
   }
 
   ngOnInit() {
-    this.getOffers();
+    this.getOffers(new HttpParams().set('category', 'Desktops'));
   }
 
-  getOffers() {
-    const params = new HttpParams().set(`category`, `Desktops`);
+  getOffers(params: HttpParams) {
     this.http.get<Offer[]>(`${environment.baseUrl}api/offers`, {params}).subscribe
     (
       {
