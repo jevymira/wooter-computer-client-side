537f52090445566ad6ec76778ab99114e1151a28
Jevy <jevymira@outlook.com>
2025-03-30 13:42:31 -0700

    refactor: remove desktops/laptops components
    Components deprecated by unified Offers component.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,9 @@
 import { Routes } from '@angular/router';
-import { DesktopsComponent } from './desktops/desktops.component';
-import { LaptopsComponent } from './laptops/laptops.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { OfferItemComponent } from './offer-item/offer-item.component';
 import { OffersComponent } from './offers/offers.component';
 
 export const routes: Routes = [
-    { path: "desktops/:category", component: DesktopsComponent },
-    { path: "laptops", component: LaptopsComponent },
     { path: "navbar", component: NavBarComponent },
     { path: "offers", component: OffersComponent },
     { path: "offer-items/:id", component: OfferItemComponent},
