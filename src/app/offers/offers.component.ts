fba39fe681b11372816fcaa96405ead26d558a56
Jevy <jevymira@outlook.com>
2025-03-30 12:39:59 -0700

    feat: route desktops, laptops to offers component
    Generate `offers.component`. Change nav-bar to pass 'Desktops' and
    'Laptops' into the `/offers` route.

---
+++ src/app/offers/offers.component.ts
@@ -0,0 +1,63 @@
+import { Component, effect, inject, OnInit, Signal } from '@angular/core';
+import { ActivatedRoute, ROUTER_OUTLET_DATA, RouterLink } from '@angular/router';
+import { Offer } from '../offer';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { environment } from '../../environments/environment.development';
+
+@Component({
+  selector: 'app-offers',
+  imports: [RouterLink],
+  templateUrl: './offers.component.html',
+  styleUrl: './offers.component.scss'
+})
+export class OffersComponent implements OnInit {
+  readonly _memory = inject(ROUTER_OUTLET_DATA) as Signal<Partial<{ has8: boolean | null; has16: boolean | null; has32: boolean | null; }>>;
+  public offers: Offer[] = [];
+
+  constructor(private http: HttpClient, private activatedRoute: ActivatedRoute) {
+    effect(() => {
+      let category = this.activatedRoute.snapshot.paramMap.get('category') || '';
+      let params = new HttpParams();
+      if (category != '') {
+        params = new HttpParams().set('category', category)
+      }
+
+      // FIXME: Scope out a more extensible way of handling
+      // FormGroup value changes.
+      if (this._memory().has8) {
+        params = params.append('memory', '8');
+      }
+      if (this._memory().has16) {
+        params = params.append('memory', '16');
+      }
+      if (this._memory().has32) {
+        params = params.append('memory', '32');
+      }
+
+      this.getOffers(params);
+    });
+  }
+
+  ngOnInit() {
+    // Reinitialize component switching between e.g., Desktops and Laptops
+    this.activatedRoute.params.subscribe(p => {
+      // `|| ...` defines default strin
+      let category = this.activatedRoute.snapshot.paramMap.get('category') || '';
+      let params = new HttpParams();
+      if (category != '') {
+        params = new HttpParams().set('category', category)
+      }
+      this.getOffers(params);
+    })
+  }
+
+  getOffers(params: HttpParams) {
+    this.http.get<Offer[]>(`${environment.baseUrl}api/offers`, {params}).subscribe
+    (
+      {
+        next: result => this.offers = result,
+        error: error => console.error(error)
+      }
+    )
+  }
+}
