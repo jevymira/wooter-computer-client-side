1927951ed8fec8b5ff9244061133fa2acc65d94e
Jevy <jevymira@outlook.com>
2025-04-26 14:32:42 -0700

    feat: add login component and auth service
    An authentication service maintains observable `authStatus`, set upon
    successful login, as invoked by `LoginComponent`. This observable
    allows reactive behavior and reactive data binding.

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { LoginRequest } from './login-request';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { LoginResponse } from './login-response';
+import { environment } from '../../environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private _authStatus = new BehaviorSubject<boolean>(false);
+  authStatus = this._authStatus.asObservable();
+
+  constructor(private http: HttpClient) { }
+
+  private setAuthStatus(value: boolean): void {
+    this._authStatus.next(value);
+  }
+
+  login(loginRequest: LoginRequest): Observable<LoginResponse> {
+    let url = `${environment.baseUrl}api/Admin/Login`;
+    return this.http.post<LoginResponse>(url, loginRequest)
+      .pipe(tap(loginResult => {
+        if (loginResult.success) {
+          localStorage.setItem('computer_comparator_jwt', loginResult.token);
+          this.setAuthStatus(true);
+        }
+      }));
+  }
+
+  logout(): void { }
+}
