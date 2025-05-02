f0dd1bcab5ef40fd0203c97706db4888027cd045
Jevy <jevymira@outlook.com>
2025-05-02 09:42:26 -0700

    feat: nav to login from bookmarks if not logged in

--- src/app/bookmarks/bookmarks.component.ts
+++ src/app/bookmarks/bookmarks.component.ts
@@ -3,7 +3,9 @@ import { BookmarksService } from './bookmarks.service';
 import { Bookmark } from './bookmark';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-bookmarks',
@@ -16,15 +18,33 @@ import { RouterLink } from '@angular/router';
   styleUrl: './bookmarks.component.scss'
 })
 export class BookmarksComponent implements OnInit{
+  private destroySubject = new Subject();
+  isLoggedIn: boolean = false;
   bookmarks: Bookmark[] = [];
 
-  constructor(private service: BookmarksService) { }
+  constructor(
+    private authService: AuthService,
+    private service: BookmarksService,
+    private router: Router) {
+    authService.authStatus.pipe(takeUntil(this.destroySubject)).subscribe(
+      result => this.isLoggedIn = result
+    )
+  }
 
   ngOnInit(): void {
-    this.service.getBookmarks(null)
-      .subscribe({
-        next: result => this.bookmarks = result,
-        error: error => console.error(error)
-      });
+    if (this.isLoggedIn) {
+      this.service.getBookmarks(null)
+        .subscribe({
+          next: result => this.bookmarks = result,
+          error: error => console.error(error)
+        });
+    } else {
+      this.router.navigate(['/login']);
+    }
+  }
+
+  ngOnDestroy() {
+    this.destroySubject.next(true);
+    this.destroySubject.complete();
   }
 }
