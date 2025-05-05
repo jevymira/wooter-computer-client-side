da0046b30d049b8cdbf34dfa351b8496ec7f42c1
Jevy <jevymira@outlook.com>
2025-05-05 10:15:54 -0700

    feat: add about component as default

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { OfferItemComponent } from './offer-item/offer-item.component';
 import { OffersComponent } from './offers/offers.component';
 import { BookmarksComponent } from './bookmarks/bookmarks.component';
 import { LoginComponent } from './auth/login.component';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
+    { path: "about", component: AboutComponent},
     { path: "login", component: LoginComponent },
     { path: "navbar", component: NavBarComponent },
     { path: "offers/:category", component: OffersComponent },
     { path: "offers/:category/:id", component: OfferItemComponent},
     { path: "bookmarks", component: BookmarksComponent},
-    { path: "", redirectTo: "offers", pathMatch: "full" }
+    { path: "", redirectTo: "about", pathMatch: "full" }
 ];
